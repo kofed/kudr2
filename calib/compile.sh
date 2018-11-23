00a3e12e89f83645290945219559ee9094fbf215
konstantin.fedulov <konstantin.fedulov@paragon-software.com>
2018-11-23 07:35:20 +0300

    .

---
+++ calib/compile.sh
@@ -0,0 +1,1 @@
+g++ -L/usr/lib/x86_64-linux-gnu/ -lopencv_features2d -lopencv_calib3d -lopencv_core -lopencv_highgui -lopencv_videoio -lopencv_imgcodecs -lopencv_imgproc camera_calibration.cpp -o calib3d
