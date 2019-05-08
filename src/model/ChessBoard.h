1bb7941741c05f1ff87d8d958522f8a926c9aaf1
kofed <kofed@yandex.ru>
2019-05-08 12:44:27 +0300

    Update ChessBoard.h
    .

--- src/model/ChessBoard.h
+++ src/model/ChessBoard.h
@@ -4,12 +4,14 @@ private:
 	//sorted corners
 	vector<vector<Point2f>> corners;
 
-	Size size;
+	
 
 public:
 	ChessBoard(const vector<vector<Point2f>> & corners);
 
 	Point2f get(Point2i index);
+	
+	const Size size;
 
 	toYml();
 
